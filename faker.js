a1bf64d938178395cfbf2afffd5d43973c69b4fd
Aqib Munir <aqibmunir724@gmail.com>
2025-08-25 10:19:05 +0500

    fixing userName to username

--- faker.js
+++ faker.js
@@ -3,7 +3,7 @@ const { faker } = require("@faker-js/faker");
 function generatePerson() {
   return {
     userId: faker.string.uuid(),
-    username: faker.internet.userName(),
+    username: faker.internet.username(),
     email: faker.internet.email(),
     avatar: faker.image.avatar(), // random avatar image
     password: faker.internet.password(),
